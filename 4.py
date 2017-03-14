1d987727cbeb418a787bb8541cd209f4d65ede4e
Boțoroga Artiom <artiombotoroga@gmail.com>
2017-03-14 19:27:47 +0400

    Phyton fisier 1.2

--- 4.py
+++ 4.py
@@ -1,5 +1,5 @@
 num_1 =  int (input("Introduceti prima cifra:"))
 num_2 = int (input("Introduceti al doilea numar:"))
-rezultat = num_1 + num_2
-rezultat += 5
+rezultat = num_1 * num_2
+rezultat += 8
 print("Rezultatul este:",rezultat)
